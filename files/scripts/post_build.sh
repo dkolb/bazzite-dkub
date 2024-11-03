ee2b5b2cfff29866c636ec09a27378968ea39fd3
David Kolb <git@dkub.dev>
2024-11-03 13:43:46 -0500

    feat: Update grub menu entry titles.

--- files/scripts/post_build.sh
+++ files/scripts/post_build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-/usr/libexec/containerbuild/image-info
+set -oue
+/usr/libexec/containerbuild/image-info &&
+KERNEL_FLAVOR=bazzite /usr/libexec/containerbuild/build-initramfs
 
 if ! grep -qE '^docker:' /etc/group; then
   grep -E '^docker:' /usr/lib/group >> /etc/group
