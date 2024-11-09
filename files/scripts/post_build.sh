e0cc032f302fd2938061d345119493d89c9dcf4f
David Kolb <git@dkub.dev>
2024-11-09 14:56:25 -0500

    wip: Move scripts plugin to containerfile snippets for better layers

--- files/scripts/post_build.sh
+++ files/scripts/post_build.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 set -oue
-/usr/libexec/containerbuild/image-info &&
-KERNEL_FLAVOR=bazzite /usr/libexec/containerbuild/build-initramfs
-
 if ! grep -qE '^docker:' /etc/group; then
   grep -E '^docker:' /usr/lib/group >> /etc/group
 fi
