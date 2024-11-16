ee54ee626f2c8a55f37bf5dfe2dda432a49ec759
David Kolb <git@dkub.dev>
2024-11-16 17:18:15 -0500

    fix: GRUB menu name. Maybe.

--- files/scripts/finalize_build.sh
+++ files/scripts/finalize_build.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 set -oue
-mv /usr/share/ublue-os/image-info.json /usr/share/ublue-os/bazzite-image-info.json
-/usr/libexec/containerbuild/image-info
-KERNEL_FLAVOR=bazzite /usr/libexec/containerbuild/build-initramfs
 
 if ! grep -qE '^docker:' /etc/group; then
   # There may not be a docker group to copy to /etc, so 
